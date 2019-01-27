e6cc84463910696a89c903f998d312852f6ae5ac
yuryklim <yuryklim@outlook.com>
2019-01-27 17:23:58 +0200

    transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {state, style, trigger} from '@angular/animations';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +13,9 @@ import {state, style, trigger} from '@angular/animations';
       state('highlighted', style({
         'background-color': 'blue',
         transform: 'translateX(100px)'
-      }))
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800))
     ])
   ]
 })
